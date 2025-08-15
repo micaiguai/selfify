66db7db9e8d90e4df01bdf9ec20c63cd02e019a9
micaiguai <1399880823@qq.com>
2025-08-15 22:15:22 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
 
-import { $ } from 'zx'
-import { type PackageJson } from 'type-fest'
+console.log('hello')
+// import { $ } from 'zx'
+// import { type PackageJson } from 'type-fest'
 
-const packageJsonRaw = await $`cat package.json`
-const json: PackageJson.PackageJsonStandard = JSON.parse(packageJsonRaw.stdout)
-const foldername = await $`basename $(pwd)`
-const gitUsername = await $`git config --get user.name`
+// const packageJsonRaw = await $`cat package.json`
+// const json: PackageJson.PackageJsonStandard = JSON.parse(packageJsonRaw.stdout)
+// const foldername = await $`basename $(pwd)`
+// const gitUsername = await $`git config --get user.name`
 
-const basename = foldername.stdout.replace('\n', '')
-const author = gitUsername.stdout.replace('\n', '')
-const description = `${basename} description`
+// const basename = foldername.stdout.replace('\n', '')
+// const author = gitUsername.stdout.replace('\n', '')
+// const description = `${basename} description`
 
-json.name = basename
-json.version = '0.0.1'
-json.description = description
-json.author = author
-const readme = `# ${basename}
-${description}
-`
+// json.name = basename
+// json.version = '0.0.1'
+// json.description = description
+// json.author = author
+// const readme = `# ${basename}
+// ${description}
+// `
 
-await $`echo ${JSON.stringify(json, null, 2)} > package.json`
-await $`echo ${readme} > README.md`
+// await $`echo ${JSON.stringify(json, null, 2)} > package.json`
+// await $`echo ${readme} > README.md`
 
-export {}
+// export {}
