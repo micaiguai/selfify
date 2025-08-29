7c4ddaebbe217a2966f60352055bc2690f472249
micaiguai <1399880823@qq.com>
2025-08-29 13:46:55 +0800

    feat: Refactor filename, dirname

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,13 @@
 import { exec } from "child_process"
 import { PathLike } from "node:fs"
 import { access } from "node:fs/promises"
+import { basename } from "node:path"
+import { fileURLToPath } from "node:url"
 import { resolve } from "path/posix"
 import { promisify } from "util"
+
+export const __filename = fileURLToPath(import.meta.url)
+export const __dirname = basename(__filename)
 
 export async function isFileExists(filePath: PathLike) {
   try {
